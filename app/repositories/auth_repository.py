2df4748da868bd313c76f30de7dfca27db7ab9ef
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-02-28 01:00:03 -0300

    feat: iniciando repository auth

--- app/repositories/auth_repository.py
+++ app/repositories/auth_repository.py
@@ -2,4 +2,15 @@
 
 class AuthRepository():
     def __init__(self):
-        pass
+        self.users = {
+            'user1': {'password': 'hashed_password1'},
+            'user2': {'password': 'hashed_password2'}
+        }
+
+    def user_exists(self, username):
+        return username in self.users
+
+    def get_user_password(self, username):
+        if self.user_exists(username):
+            return self.users[username]['password']
+        return None
