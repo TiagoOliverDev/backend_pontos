efdf932031f12c2324887c582f4b84e231fc18b6
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-02-29 00:41:56 -0300

    breaking_changes: refatorando service

--- app/services/collaborator_service.py
+++ app/services/collaborator_service.py
@@ -1,7 +1,4 @@
-# service.py
-from passlib.hash import bcrypt
 from ..repositories.collaborator_repository import CollaboratorRepository
-import jwt
 import logging
 
 
@@ -21,12 +18,15 @@ class CollaboratorService:
             logging.error(f"Error fetching collaborators: {e}")
             return {'message': 'Internal server error'}, 500
         
-    def register_collaborator(self, name: str):
+    def register_collaborator(self, name: str, email: str, password: str, matricula: str, tipo_permissao: int):
         try:
-            if collaborator_repository.collaborator_exists(name=name):
-                return None, 'collaborator already exists'
+            if collaborator_repository.collaborator_email_exists(email):
+                return None, 'email collaborator already exists'
+            
+            if collaborator_repository.collaborator_matricula_exists(matricula):
+                return None, 'matricula collaborator already exists'
 
-            collaborator_created = collaborator_repository.collaborator_create(name=name)
+            collaborator_created = collaborator_repository.collaborator_create(name, email, password, matricula, tipo_permissao)
             return collaborator_created, None
         except Exception as e:
             logging.error(f"Error registering collaborator: {e}")
@@ -40,9 +40,9 @@ class CollaboratorService:
             logging.error(f"Error list collaborator: {e}")
             return None, 'Error list collaborator. Please try again later.'
     
-    def update_collaborator(self, id: int, name: str):
+    def collaborator_update(self, id: int, **kwargs):
         try:
-            collaborator_edited = collaborator_repository.update_collaborator(id=id, new_name=name)
+            collaborator_edited = collaborator_repository.update_collaborator(id=id, **kwargs)
             return collaborator_edited, None
         except Exception as e:
             logging.error(f"Error edit collaborator: {e}")
