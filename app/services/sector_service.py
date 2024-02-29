b6e064573c4fea4bca5c3205596c8decae60550c
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-02-28 21:48:54 -0300

    feat: criando service do sector

---
+++ app/services/sector_service.py
@@ -0,0 +1,58 @@
+# service.py
+from passlib.hash import bcrypt
+from ..repositories.sector_repository import SectorRepository
+import jwt
+import logging
+
+
+sector_repository = SectorRepository()
+
+class SectorService:
+    def __init__(self, secret_key):
+        self.secret_key = secret_key
+        
+    def list_all_sectors(self):
+        try:
+            sectors = sector_repository.list_all_sectors()
+            if sectors is None:
+                return {'message': 'Error fetching sectors'}, 500
+            return {'sectors': sectors}, 200
+        except Exception as e:
+            logging.error(f"Error fetching sectors: {e}")
+            return {'message': 'Internal server error'}, 500
+        
+    def register_sector(self, name: str):
+        try:
+            if sector_repository.sector_exists(name=name):
+                return None, 'Sector already exists'
+
+            sector_created = sector_repository.sector_create(name=name)
+            return sector_created, None
+        except Exception as e:
+            logging.error(f"Error registering sector: {e}")
+            return None, 'Error registering sector. Please try again later.'
+    
+    def list_sector_by_id(self, id: int):
+        try:
+            sector = sector_repository.list_sector_by_id(id=id)
+            return sector, None
+        except Exception as e:
+            logging.error(f"Error list sector: {e}")
+            return None, 'Error list sector. Please try again later.'
+    
+    def update_sector(self, id: int, name: str):
+        try:
+            sector_edited = sector_repository.update_sector(id=id, new_name=name)
+            return sector_edited, None
+        except Exception as e:
+            logging.error(f"Error edit sector: {e}")
+            return None, 'Error edit sector. Please try again later.'
+        
+    def sector_delete(self, id: int):
+        try:
+            sector_excluded = sector_repository.sector_delete(id=id)
+            return sector_excluded, None
+        except Exception as e:
+            logging.error(f"Error in excluded sector: {e}")
+            return None, 'Error in excluded sector. Please try again later.'
+
