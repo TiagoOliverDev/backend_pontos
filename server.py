94d353beb092974039f07dbd156e2580b884a74f
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-04-07 15:26:05 -0300

    feat: jogando iniciação do backend dentro de uma função

--- server.py
+++ server.py
@@ -5,24 +5,23 @@ from flask_cors import CORS
 import logging
 import os
 
-app = Flask(__name__)
-CORS(app) 
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
 
+    # Carregar configuração com base no ambiente
+    if os.environ.get('FLASK_ENV') == 'production':
+        app.config.from_object('config.production')
+        logging.info('PRODUÇÃO')
+    else:
+        app.config.from_object('config.development')
+        logging.info('DESENVOLVIMENTO')
 
-# Carregar configuração com base no ambiente
-if os.environ.get('FLASK_ENV') == 'production':
-    app.config.from_object('config.production')
-    logging.info('PRODUÇÃO')
-else:
-    app.config.from_object('config.development')
-    logging.info('DESENVOLVIMENTO')
-
-bcrypt = Bcrypt(app)
-app.register_blueprint(routes_blueprint)
+    bcrypt = Bcrypt(app)
+    app.register_blueprint(routes_blueprint)
+    
+    return app
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(host="0.0.0.0", port=1010, debug=app.config['DEBUG'])
-
-
-
-
